f6a55c76f84787733f1f404be23a4f2388e4e63a
Scott  Keplinger <skeplin17@gmail.com>
2020-05-21 03:14:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -36,35 +36,3 @@ class TriangleError < StandardError
 end
 
 end
-#
-# end
-# def kind
-#   if @side_a == @side_b && @side_b == @side_c
-#       :equilateral
-#     elsif @side_a == @side_b||@side_c && @legth_b == @side_a||@side_c
-#       :isosceles
-#     elsif @side_a != @side_b && @side_b != @side_c
-#       :scalene
-#     elsif @side_a + @side_b < @side_c || @side_b + @side_c < @side_a
-#       raise TriangleError
-#     elsif @side_a * @side_b * @side_c == 0
-#       raise TriangleError
-#     else
-#   end
-# end
-
-# def kind
-#   triangle = self
-#   case triangle
-#   when @side_a == @side_b && @side_b == @side_c
-#       :equilateral
-#     when @side_a == @side_b||@side_c || @legth_b == @side_a||@side_c
-#       :isosceles
-#     when @side_a + @side_b < @side_c || @side_b + @side_c < @side_a
-#       raise TriangleError
-#     when @side_a * @side_b * @side_c == 0
-#       raise TriangleError
-#     else
-#       :scalene
-#   end
-# end
