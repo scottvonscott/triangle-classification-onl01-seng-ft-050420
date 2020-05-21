61b99a03449380476397adfb5c91c4827234ab05
Scott  Keplinger <skeplin17@gmail.com>
2020-05-21 02:01:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,49 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+attr_accessor :side_a, :side_b, :side_c
+
+
+def initialize (side_a, side_b, side_c)
+  @side_a = side_a
+  @side_b = side_b
+  @side_c = side_c
 end
+
+def kind
+  if @side_a == @side_b && @side_b == @side_c
+      :equilateral
+    elsif @side_a == @side_b||@side_c || @legth_b == @side_a||@side_c
+      :isosceles
+    elsif @side_a + @side_b < @side_c || @side_b + @side_c < @side_a
+      raise TriangleError
+    elsif @side_a * @side_b * @side_c == 0
+      raise TriangleError
+    else
+      :scalene
+  end
+end
+
+
+class TriangleError < StandardError
+end
+
+end
+
+
+# def kind
+#   triangle = self
+#   case triangle
+#   when @side_a == @side_b && @side_b == @side_c
+#       :equilateral
+#     when @side_a == @side_b||@side_c || @legth_b == @side_a||@side_c
+#       :isosceles
+#     when @side_a + @side_b < @side_c || @side_b + @side_c < @side_a
+#       raise TriangleError
+#     when @side_a * @side_b * @side_c == 0
+#       raise TriangleError
+#     else
+#       :scalene
+#   end
+# end
