f39bebba9adc76f75f72a33445e321ab8668da26
Scott  Keplinger <skeplin17@gmail.com>
2020-05-21 03:19:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,8 @@ def kind
   bad_triangle = @side_array.sort
   if @side_array.min <= 0
     raise TriangleError
-  elsif
+  elsif bad_triangle[0] + bad_triangle[1] > bad_triangle[2]
+    raise TriangleError
   elsif triangle == 1
     :equilateral
   elsif triangle == 2
