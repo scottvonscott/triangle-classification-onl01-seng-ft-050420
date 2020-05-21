a93b4c7c54b06ab4e69461678177385247664ca0
Scott  Keplinger <skeplin17@gmail.com>
2020-05-21 02:10:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,11 +14,10 @@ end
 def kind
   if @side_a == @side_b && @side_b == @side_c
       :equilateral
-    elsif @side_a != @side_b && @side_b != @side_c
-      :scalene
     elsif @side_a == @side_b||@side_c && @legth_b == @side_a||@side_c
       :isosceles
-
+    elsif @side_a != @side_b && @side_b != @side_c
+      :scalene
     elsif @side_a + @side_b < @side_c || @side_b + @side_c < @side_a
       raise TriangleError
     elsif @side_a * @side_b * @side_c == 0
